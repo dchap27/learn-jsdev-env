881bb2209253a7e8637dcd03f7ee1090129196fb
Ahmad Adelaja <ahmadadelaja@gmail.com>
2020-04-28 18:49:45 +0100

    Add .babelrc and configure package.json scripts to use babel-node

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,1 +1,5 @@
-console.log("Starting app in dev mode")
+// var chalk = require("chalk");
+// use ES6 code instead of 'require' after babel configuration
+import chalk from "chalk";
+
+console.log(chalk.green("Starting app in dev mode..."));
